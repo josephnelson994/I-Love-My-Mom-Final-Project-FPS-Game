52af4eee01c68ffc8754f2eef4683e3f162862b0
Guinevere <myfriendgibuttersnaps@gmail.com>
2026-04-22 16:09:59 -0600

    More footstep work not 100 percent working, but i have fixed some wwise stuff

--- Assets/Script/Footstepsbby.cs
+++ Assets/Script/Footstepsbby.cs
@@ -1,96 +1,29 @@
 using UnityEngine;
-private enum CURRENT_TERRAIN { CARPET, METAL, RAMP, WATER };
-[SerializeField]
-private CURRENT_TERRAIN currentTerrain;
 
-[SerializeField]
-private AK.Wwise.Event footstepsEvent;
-
-[SerializeField]
-private AK.Wwise.Switch[] terrainSwitch;
-
-  private void CheckTerrain()
-    {
-        RaycastHit[] hit;
-
-        hit = Physics.RaycastAll(transform.position, Vector3.down, 10.0f);
-
-        foreach (RaycastHit rayhit in hit)
-        {
-            if (rayhit.transform.gameObject.layer == LayerMask.NameToLayer("Carpet"))
-            {
-                currentTerrain = CURRENT_TERRAIN.CARPET;
-            }
-            else if (rayhit.transform.gameObject.layer == LayerMask.NameToLayer("Metal"))
-            {
-                currentTerrain = CURRENT_TERRAIN.METAL;
-            }
-            else if (rayhit.transform.gameObject.layer == LayerMask.NameToLayer("Ramp"))
-            {
-                currentTerrain = CURRENT_TERRAIN.RAMP;
-            }
-            else if (rayhit.transform.gameObject.layer == LayerMask.NameToLayer("Water"))
-            {
-                currentTerrain = CURRENT_TERRAIN.WATER;
-            }
-        }
-    }
-private void Update() 
-    {
-        CheckTerrain();
-    }
-private void PlayFootstep(int terrain)
-    {
-       terrainSwitch[terrain].SetValue(this.gameObject);
-       AkSoundEngine.PostEvent(footstepsEvent.Id, this.gameObject);
-    }
-public void SelectAndPlayFootstep()
-    {     
-        switch (currentTerrain)
-        {
-            case CURRENT_TERRAIN.CARPET:
-                PlayFootstep(0);
-                break;
-
-            case CURRENT_TERRAIN.METAL:
-                PlayFootstep(1);
-                break;
-
-            case CURRENT_TERRAIN.RAMP:
-                PlayFootstep(2);
-                break;
-
-            case CURRENT_TERRAIN.WATER:
-                PlayFootstep(3);
-                break;
-
-            default:
-                PlayFootstep(0);
-                break;
-        }
-    }
-private Rigidbody rb;
-
-float timer = 0.0f;
-
-[SerializeField]
-float footstepSpeed = 0.3f;
-private void Awake()
+public class PlayerFootsteps : MonoBehaviour
 {
-        rb = GetComponent<Rigidbody>();
-}
-private void Update()
-    {
-        CheckTerrain();
+    public AK.Wwise.Event footstepEvent;
+    public float walkStepDistance = 2.0f;
+    private float distanceTraveled = 0f;
+    private Vector3 lastPosition;
 
-        if (rb.velocity.magnitude > 1f && Mathf.Abs(rb.velocity.y) < 0.1f)
-        {
-            if (timer > footstepSpeed)
-            {
-                SelectAndPlayFootstep();
-                timer = 0.0f;
-            }
-
-            timer += Time.deltaTime;
-        }
+    void Start() {
+        lastPosition = transform.position;
     }
+
+    void Update() {
+        float distanceMove = Vector3.Distance(transform.position, lastPosition);
+        distanceTraveled += distanceMove;
+
+        if (distanceTraveled >= walkStepDistance) {
+            PlayFootstep();
+            distanceTraveled = 0f;
+        }
+        lastPosition = transform.position;
+    }
+
+    void PlayFootstep() {
+        // Trigger the Wwise event on this GameObject
+        footstepEvent.Post(gameObject); 
+    }
+}
